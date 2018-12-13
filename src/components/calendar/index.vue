8ece786adaf76fc41fd0df62d4710c4412a87561
dyyaoGit <646617513@qq.com>
2018-12-13 09:00:33 +0800

    日历问题修复

--- src/components/calendar/index.vue
+++ src/components/calendar/index.vue
@@ -3,7 +3,7 @@
     <div class="house-title">
         七日化房间管理
         <span class="house-title-date">
-          2018-08-01
+          {{dateData.formatTime}}
         </span>
     </div>
     <table class="main-table" @click="handleClick">
@@ -15,12 +15,12 @@
             <div class="housetype-text">房型</div>
             <i class="line" ref="line"></i>
           </td>
-          <td v-for="item in 7" class="cell-item">
+          <td v-for="(item, index) in dateData.weekDate" class="cell-item">
             <div class="cell-top">
-              16
+              {{item}}
             </div>
             <div class="cell-bottom">
-              星期一
+              {{week[index]}}
             </div>
           </td>
         </tr>
@@ -28,7 +28,7 @@
           <td class="row-title" >
             {{item}}
           </td>
-          <td class="item-num" v-for="(item,index) in 7" :data-row="idx">
+          <td class="item-num" v-for="(item,index) in 7" :data-row="idx" :data-col="index">
             {{idx+index}}
           </td>
         </tr>
@@ -38,6 +38,7 @@
 </template>
 
 <script>
+  import moment from 'moment'
   import dyDialog from '../dy-dialog'
 
   export default {
@@ -54,8 +55,14 @@
           time: '2018-10-20',
           houseType: '豪华大床房'
         },
+        dateData: {
+          formatTime: '', // 格式化好的日期
+          month: '', // 当前月份
+          weekDate: [] // 一星期的数组
+        },
+        week: ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日',],
         testData: {
-          time: 1544350369753,
+          time: 1544629397, // unix时间戳
           bigBed: [
             {
               num: 2,
@@ -136,8 +143,8 @@
       handleClick(e) {
         let target = e.target.classList.contains('item-num')&&e.target;
         if(target) {
-          console.log(target)
-          console.log(target.dataset.row)
+          console.log(target.dataset.col + '列')
+          console.log(target.dataset.row + '行')
         }
       },
       handleClose() {
@@ -155,6 +162,35 @@
 
           line.style.width = hypotenuse + '100px'
           line.style.transform = `translate(-50%, -50%) rotateZ(${rotateDeg}deg)`;
+      },
+      getDate () {
+        let unix = this.testData.time //unix 时间戳
+        let minDate = new Date(unix*1000) // /用unix时间戳得到时间对象
+        let today = minDate.getDay() // 获取星期几
+        let todayNum = minDate.getDate() // 今天是多少号
+        let firstDay = -1; // 这一周开始的第一天
+        if(today == 0) { // 如果是星期天
+          let preDay = todayNum - 6;
+          minDate.setDate(preDay)
+          firstDay = minDate.getDate(); // 赋值第一天
+          this.dateData.weekDate = []
+          for(let i = 0; i<7; i++) {
+            this.dateData.weekDate.push(firstDay + i)
+          }
+
+        } else { // 如果不是星期天
+          let preDay = todayNum - today + 1; // 得到这周开始的号数
+          minDate.setDate(preDay) // 设置这周开始的号数
+          // console.log(minDate.getDate()); //得到了计算好的这周开始的号数
+          firstDay = minDate.getDate(); // 赋值第一天
+          console.log(firstDay);
+          this.dateData.weekDate = []
+          for(let i = 0; i<7; i++) {
+            this.dateData.weekDate.push(firstDay + i)
+          }
+        }
+
+
       }
     },
     mounted() {
@@ -165,6 +201,7 @@
           vm.setLine()
         }
       })
+      this.getDate()
     },
     beforeDestroy () {
       window.onresize = null
